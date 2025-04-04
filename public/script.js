af9be18efce7e9782e3697170fdc33dfe664e6c9
cstro <santiagodanielcastro05@gmail.com>
2025-04-04 19:07:50 -0300

    v1.0.0 (funcional)

--- public/script.js
+++ public/script.js
@@ -23,12 +23,74 @@ document.getElementById("formActualizarStock").addEventListener("submit", async 
             console.error("Error al actualizar stock:", data.error);
             alert("Error al actualizar stock");
             document.getElementById("modalStock").style.display = "none";
+            await cargarDrogas();
         }
     } catch (error) {
         console.error("Error en la solicitud:", error);
         document.getElementById("modalStock").style.display = "none";
+        await cargarDrogas();
     }
 });
+
+document.getElementById("modalAgregar").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById("nombreDroga").value; // Corregido
+    const stock = parseInt(document.getElementById("cantStock").value);
+
+    try {
+        const response = await fetch("http://localhost:4000/crear", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ nombre, stock })
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            console.log(data.message + " | ID: " + data.id);
+            alert(data.message);
+            document.getElementById("modalAgregar").style.display = "none";
+            await cargarDrogas();
+        } else {
+            console.error("Error al crear droga:", data.error);
+            alert("Error al crear droga");
+            document.getElementById("modalAgregar").style.display = "none";
+        }
+    } catch (error) {
+        console.error("Error en la solicitud:", error);
+        document.getElementById("modalAgregar").style.display = "none";
+    }
+    document.getElementById("nombreDroga").value = ""
+    document.getElementById("cantStock").value = ""
+});
+
+async function eliminarDroga(id){
+    if(!confirm("Estas seguro wachinaso?")) return;
+    try {
+        const response = await fetch(`http://localhost:4000/eliminar/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            console.log(data.message + " | ID: " + data.id);
+            alert(data.message);
+            await cargarDrogas();
+        } else {
+            console.error("Error al eliminar droga:", data.error);
+            alert("Error al eliminar droga");
+            await cargarDrogas();
+        }
+    } catch (error) {
+        console.error("Error en la solicitud:", error);
+        await cargarDrogas();
+    }
+}
 
 function modificarStock(stock, id){
     document.getElementById("modificarStock").value = stock;
@@ -36,8 +98,17 @@ function modificarStock(stock, id){
     document.getElementById("modalStock").style.display = "flex";
 }
 
+function agregarDroga(){
+    document.getElementById("modalAgregar").style.display = "flex";
+}
+
 document.getElementById("closeModal").addEventListener("click", function() {
     document.getElementById("modalStock").style.display = "none";
+    document.getElementById("modalAgregar").style.display = "none";
+});
+
+document.getElementById("closeAgregarDroga").addEventListener("click", function() {
+    document.getElementById("modalAgregar").style.display = "none";
 });
 
 async function cargarDrogas(){
@@ -60,7 +131,7 @@ async function cargarDrogas(){
                 <td>${d.id}</td>
                 <td class="arrNombre">${d.nombre}</td>
                 <td>${d.stock}</td>
-                <td><button class="optButton btnModificarStock" onclick="modificarStock(${d.stock},${d.id})">✏️</button> <button class="optButton btnEliminar">✕</button></td>
+                <td><button class="optButton btnModificarStock" onclick="modificarStock(${d.stock},${d.id})">✏️</button> <button class="optButton btnEliminar" onclick="eliminarDroga(${d.id})">✕</button></td>
             </tr>
                 `
             });
@@ -77,5 +148,6 @@ async function cargarDrogas(){
 
 window.addEventListener("load", async function(){
     document.getElementById("modalStock").style.display = "none";
+    document.getElementById("modalAgregar").style.display = "none";
     await cargarDrogas();
 })
