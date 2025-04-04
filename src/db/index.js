38b90fc4f4f5406eb20669d8da485050f074f8b9
cstro <santiagodanielcastro05@gmail.com>
2025-04-04 20:44:14 -0300

    v1.0.2 - DB Fixed

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./db/database.sqlite3'); // o donde tengas tu DB
+const db = new sqlite3.Database('./stock.db'); // o donde tengas tu DB
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS weed (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT UNIQUE,
+        stock INTEGER
+    )`);
+});
 
 module.exports = db;
