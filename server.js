cc1ed7089ac90a51114f7a82b760a63f19ee6c12
cstro <santiagodanielcastro05@gmail.com>
2025-04-06 15:13:26 -0300

    Test >> Ulpload arhives v2

--- server.js
+++ server.js
@@ -8,28 +8,6 @@ const server = http.createServer(app);
 setupSocket(server);
 
 const PORT = process.env.PORT || 4000;
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // carpeta donde se guarda
-    },
-    filename: function (req, file, cb) {
-        const uniqueName = Date.now() + '-' + file.originalname;
-        cb(null, uniqueName);
-    }
-});
-
-const upload = multer({ storage: storage });
-
-app.post('/subir', upload.single('archivo'), (req, res) => {
-    console.log(req.file); // info del archivo
-    res.send('Archivo subido correctamente ✔️');
-});
-
-// Carpeta de subida accesible
-app.use('/uploads', express.static('uploads'));
-
-
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -2,12 +2,35 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const drogaRoutes = require('./routes/drogas.routes');
+const multer = require("multer");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // carpeta donde se guarda
+    },
+    filename: function (req, file, cb) {
+        const uniqueName = Date.now() + '-' + file.originalname;
+        cb(null, uniqueName);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/subir', upload.single('archivo'), (req, res) => {
+    console.log(req.file); // info del archivo
+    res.send('Archivo subido correctamente ✔️');
+});
+
+// Carpeta de subida accesible
+app.use('/uploads', express.static('uploads'));
+
+
 
 app.use('/', drogaRoutes);
 
