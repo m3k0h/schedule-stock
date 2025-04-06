e0d77e1c729be451079de0140f8413daae2b8736
cstro <santiagodanielcastro05@gmail.com>
2025-04-06 15:34:53 -0300

    Test >> Arreglando la cagada del v3

--- src/app.js
+++ src/app.js
@@ -8,10 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
-
-app.use('/uploads', express.static('uploads'));
 app.use('/', drogaRoutes);
 
-module.exports = {
-    app
-};
+module.exports = app;
--- src/routes/drogas.routes.js
+++ src/routes/drogas.routes.js
@@ -1,19 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/drogas.controller');
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, '../../uploads/');
-    },
-    filename: function (req, file, cb) {
-        const uniqueName = Date.now() + '-' + file.originalname;
-        cb(null, uniqueName);
-    }
-});
-
-const upload = multer({ storage: storage });
 
 router.get('/stock/:nombre', controller.obtenerStock);
 router.post('/venta', controller.registrarVenta);
@@ -22,5 +9,4 @@ router.post('/crear', controller.crearDroga);
 router.get('/drugs', controller.obtenerDrogas);
 router.delete('/eliminar/:id', controller.eliminarDroga);
 router.get('/health', (req, res) => res.send('OK'));
-router.post('/subir', upload.single('archivo'), controller.subirArchivo)
 module.exports = router;
